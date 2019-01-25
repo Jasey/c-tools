cabbdddfa4e63f8959b8288b43483e3ba9a85ffa
jasey <jasey@xmlydeMacBook-Pro-3.local>
2019-01-25 19:11:18 +0800

    mbr

--- mbr-lba/mbr-lba.c
+++ mbr-lba/mbr-lba.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MBR_LEN 521
+#define MBR_LEN 512
 
 int main(int argc, char const *argv[])
 {
+    int startSeek = 0;
     if (argv[1] == NULL)
     {
         printf("please enter binary file!\n");
@@ -16,6 +17,14 @@ int main(int argc, char const *argv[])
     {
         printf("please enter vhd file!\n");
         return 0;
+    }
+
+    if (argv[3] == NULL)
+    {
+        printf("--> not set mbr logic addr, defualt write to lba 0\n");
+    }else
+    {
+        startSeek = atoi(argv[3]);
     }
 
     const char* asmFileName = argv[1];
@@ -40,12 +49,17 @@ int main(int argc, char const *argv[])
         printf("open file \"%s\" failed!\n", diskFileName);
         exit(1);
     }
-    fseek(desFd, 0, SEEK_SET);
+    if (fseek(desFd, startSeek*MBR_LEN, SEEK_SET) != 0)
+    {
+        printf("can't find lba addr : %d\n", startSeek);
+        exit(1);
+    }
+
     fwrite(binArray, sizeof(char), MBR_LEN, desFd);
 
     fclose(desFd);
 
-    printf("write mbr success!!\n");
+    printf("write mbr lba addr : %d success!!\n", startSeek);
 
     return 0;
 
